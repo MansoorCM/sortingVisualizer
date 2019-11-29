95dfb28b031fc4af29399abc4e04b544de8e7ff3
DELL <manzoorcm96@gmail.com>
2019-11-29 14:27:20 +0530

    Heap sort implemented. method to swap images improved

--- app/src/main/java/com/example/practice/SortAlgActivity.java
+++ app/src/main/java/com/example/practice/SortAlgActivity.java
@@ -19,7 +19,6 @@ public class SortAlgActivity extends AppCompatActivity {
     ImageView number1,number2,number3,number4,number5,number6,number7,number8,number9,number10;
     int[] data={520,870,1020,410,1190,321,665,989,432,1000};
     View[] image;
-    int k;
     Handler handler;
     Handler inhandler;
     String algo;
@@ -73,16 +72,15 @@ public class SortAlgActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
 
     }
-    void bubbleSort(final int arr[]) {
+
+    void bubbleSort(final int[] arr) {
         final int n = arr.length;
-        final ViewGroup viewGroup = (ViewGroup) number4.getParent();
         for (int b = 0; b < n - 1; b++) {
             final int i = b;
             handler.postDelayed(new Runnable() {
                 @Override
                 public void run() {
-                    String s= "iteration "+String.valueOf(i);
-                    Toast.makeText(SortAlgActivity.this, s, Toast.LENGTH_SHORT).show();
+
                     for (int c = 0; c < n - i - 1; c++)
                     {
                         final int j=c;
@@ -94,37 +92,20 @@ public class SortAlgActivity extends AppCompatActivity {
                                     int temp = arr[j];
                                     arr[j] = arr[j + 1];
                                     arr[j + 1] = temp;
-                                    k = j;
-                                    viewGroup.removeView(image[k]);
-                                    k += 1;
-                                    while (k < n) {
-                                        viewGroup.removeView(image[k]);
-                                        k += 1;
-                                    }
-
-                                    viewGroup.addView(image[j + 1]);
-                                    viewGroup.addView(image[j]);
-                                    k = j + 2;
-                                    while (k < n) {
-                                        viewGroup.addView(image[k]);
-                                        k += 1;
-                                    }
-                                    View view = image[j];
-                                    image[j] = image[j + 1];
-                                    image[j + 1] = view;
+                                    image[j].requestLayout();
+                                    int height=image[j].getLayoutParams().height;
+                                    image[j+1].requestLayout();
+                                    image[j].getLayoutParams().height=image[j+1].getLayoutParams().height;
+                                    image[j+1].getLayoutParams().height=height;
 
                                 }
 
                             }
                         },500*j);
-                        // image[j].setBackgroundColor(getResources().getColor(R.color.special));
                     }
                 }
 
             }, 1000*i);
-
-            String res = String.format("%d %d %d %d %d", arr[0], arr[1], arr[2], arr[3], arr[4]);
-            Log.i("sorted array", res);
         }
     }
 
@@ -273,6 +254,151 @@ public class SortAlgActivity extends AppCompatActivity {
 
     }
 
+
+        void merge(int arr[], int l, int m, int r)
+        {
+            // Find sizes of two subarrays to be merged
+            int n1 = m - l + 1;
+            int n2 = r - m;
+
+            /* Create temp arrays */
+            int L[] = new int [n1];
+            int R[] = new int [n2];
+
+            /*Copy data to temp arrays*/
+            for (int i=0; i<n1; ++i)
+                L[i] = arr[l + i];
+            for (int j=0; j<n2; ++j)
+                R[j] = arr[m + 1+ j];
+
+
+            /* Merge the temp arrays */
+
+            // Initial indexes of first and second subarrays
+            int i = 0, j = 0;
+
+            // Initial index of merged subarry array
+            int k = l;
+            while (i < n1 && j < n2)
+            {
+                if (L[i] <= R[j])
+                {
+                    arr[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    arr[k] = R[j];
+                    j++;
+                }
+                k++;
+            }
+
+            /* Copy remaining elements of L[] if any */
+            while (i < n1)
+            {
+                arr[k] = L[i];
+                i++;
+                k++;
+            }
+
+            /* Copy remaining elements of R[] if any */
+            while (j < n2)
+            {
+                arr[k] = R[j];
+                j++;
+                k++;
+            }
+        }
+
+        // Main function that sorts arr[l..r] using
+        // merge()
+        void Mergesort(int arr[], int l, int r)
+        {
+            if (l < r)
+            {
+                // Find the middle point
+                int m = (l+r)/2;
+
+                // Sort first and second halves
+                Mergesort(arr, l, m);
+                Mergesort(arr ,m+1, r);
+
+                // Merge the sorted halves
+                merge(arr, l, m, r);
+            }
+        }
+
+    public void heapSort(final int[] arr)
+    {
+        int n = arr.length;
+
+        // Build heap (rearrange array)
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
+
+        // One by one extract an element from heap
+        for (int b=n-1; b>=0; b--)
+        {
+            final int i=b;
+            handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                int temp = arr[0];
+                arr[0] = arr[i];
+                arr[i] = temp;
+                image[i].requestLayout();
+                int height=image[i].getLayoutParams().height;
+                Log.i("value", String.valueOf(height));
+                image[0].requestLayout();
+                image[i].getLayoutParams().height=image[0].getLayoutParams().height;
+                Log.i("value", String.valueOf(image[0].getLayoutParams().height));
+                image[0].getLayoutParams().height=height;
+
+                // call max heapify on the reduced heap
+                heapify(arr, i, 0);
+                // Move current root to end
+            }
+        },(n-b)*1000);
+
+        }}
+
+    void heapify(int[] arr, int n, int i)
+    {
+        int largest = i; // Initialize largest as root
+        int l = 2*i + 1; // left = 2*i + 1
+        int r = 2*i + 2; // right = 2*i + 2
+
+        // If left child is larger than root
+        if (l < n && arr[l] > arr[largest])
+            largest = l;
+
+        // If right child is larger than largest so far
+        if (r < n && arr[r] > arr[largest])
+            largest = r;
+
+        // If largest is not root
+        if (largest != i)
+        {
+            int swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+            image[i].requestLayout();
+            int height=image[i].getLayoutParams().height;
+            image[largest].requestLayout();
+            image[i].getLayoutParams().height=image[largest].getLayoutParams().height;
+            image[largest].getLayoutParams().height=height;
+
+            // Recursively heapify the affected sub-tree
+            heapify(arr, n, largest);
+        }
+    }
+
+    void quickSort()
+    {
+
+    }
+
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         if(item.getItemId()==R.id.sort)
@@ -288,6 +414,18 @@ public class SortAlgActivity extends AppCompatActivity {
             else if(algo.equals("Insertion Sort"))
             {
                 InsertionSort(data);
+            }
+            else if(algo.equals("Merge Sort"))
+            {
+                Mergesort(data,0,data.length-1);
+            }
+            else if(algo.equals("Heap Sort"))
+            {
+                heapSort(data);
+            }
+            else if(algo.equals("Quick Sort"))
+            {
+                quickSort();
             }
             else
             {
